5aee41f13df9da657d501626767f48af98a645b7
99003713 <harodharod2018@gmail.com>
2021-04-10 23:17:20 +0530

    updated

---
+++ Learning/index1.js
@@ -0,0 +1,39 @@
+console.time('Your code took');
+
+console.log('omprakash harod');
+console.log(10);
+console.log(10+20);
+console.log([10,20,'omprakash',null, undefined]);
+console.log({
+    key1: 1,
+    key2: 'omprakash'
+});
+
+console.time('mytime');
+console.table({
+    key1: 1,
+    key2: 'omprakash'
+});
+
+console.warn({
+    key1: 1,
+    key2: 'omprakash'
+});
+
+console.error({
+    key1: 1,
+    key2: 'omprakash'
+});
+
+// console.clear();
+console.timeEnd('mytime');
+
+console.timeEnd('Your code took');
+
+console.assert(10>11, 'this will give error');
+
+/*
+hi 
+om 
+mulit line comment
+*/
